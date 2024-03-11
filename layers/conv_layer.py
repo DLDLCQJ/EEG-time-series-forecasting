fba3c603871fe8a8b739d7d1e07e60c6d4217858
Simon_Wang <145650040+DLDLCQJ@users.noreply.github.com>
2024-03-11 15:05:16 +0800

    Create conv_layer.py

---
+++ layers/conv_layer.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+
+class ConvLayer(nn.Module):
+    def __init__(self, c_in):
+        super(ConvLayer, self).__init__()
+        padding = 1 if torch.__version__>='1.5.0' else 2
+        self.downConv = nn.Conv1d(in_channels=c_in,
+                                  out_channels=c_in,
+                                  kernel_size=3,
+                                  padding=padding,
+                                  padding_mode='circular')
+        self.norm = nn.BatchNorm1d(c_in)
+        self.activation = nn.ELU()
+        self.maxPool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        x = self.downConv(x.permute(0, 2, 1))
+        x = self.norm(x)
+        x = self.activation(x)
+        #print(x.shape)
+        x = self.maxPool(x)
+        #print(x.shape)
+        x = x.transpose(1,2)
+        return x
+        
