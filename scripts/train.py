4176da8a906c72bef385b33e66d0241fd618fe6e
Simon_Wang <145650040+DLDLCQJ@users.noreply.github.com>
2024-03-11 14:53:26 +0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,22 +1,19 @@
 
-def train_epoch(optim_dict,model_dict,x,h0,y,loss_MSE,loss_KLD,latent_dim):    
-    #####VAE########
-    loss_dict = {'V': [], 'EG': []}
-    optim_dict["V"].zero_grad()
-    vae_out = model_dict["V"](x,h0) #!!!
-    loss_re = loss_MSE(vae_out[0], x) 
-    loss_norm = loss_KLD(vae_out[2], vae_out[3]) 
-    loss_V = loss_re + loss_norm
-    loss_V.backward()
-    optim_dict["V"].step()
-    loss_dict["V"].append(loss_V.data.item())
+def train_epoch(optim_dict,model_dict,x,h,y,loss_MSE,loss_KLD,tw, pred):    
+    bs=x.shape[0]
+    optim_dict["DA"].zero_grad()
+    optim_dict["F"].zero_grad()
+    out_E = model_dict["E"](x,h)  
+    recon_out_attn = model_dict["DA"](out_E)  
+    g_out = model_dict["G"](out_E)
+    pred_data = model_dict["F"](g_out) ##mean
+    loss_re_attn = loss_MSE(recon_out_attn, x) 
+    #loss_norm_attn = loss_KLD(out_E[0], out_E[1]) 
+    loss_eg = loss_MSE(pred_data, y)
+    DA_loss_attn = loss_re_attn
+    DA_loss_attn.backward(retain_graph=True)
+    loss_eg.backward()
+    optim_dict["DA"].step()
+    optim_dict["F"].step()
 
-    #EG
-    optim_dict["EG"].zero_grad()
-    pred_data = model_dict["G"](model_dict["V"].encoder(x, h0)[1].reshape(batch_size,latent_dim,1)) ##mean
-    pred_data = torch.cat([y[:, :6801, :], pred_data.reshape(-1, 6801//3, 1)], axis = 1)
-    loss_EG = loss_MSE(pred_data, y)
-    loss_EG.backward()
-    optim_dict["EG"].step()
-    loss_dict["EG"].append(loss_EG.data.item())
-    return loss_dict, pred_data
+    return loss_eg, recon_out_attn,pred_data
